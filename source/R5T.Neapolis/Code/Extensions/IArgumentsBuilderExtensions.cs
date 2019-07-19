a670fcca9ec4c4a5bda5dc03785fce1c3fd97ae8
MinexAutomation <minex.automation@gmail.com>
2019-07-19 12:23:20 -0400

    Added extensions using the strong-typed paths from R5T.NetStandard.IO.Paths.

---
+++ source/R5T.Neapolis/Code/Extensions/IArgumentsBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using R5T.NetStandard.IO.Paths;
+
+
+namespace R5T.Neapolis
+{
+    public static class IArgumentsBuilderExtensions
+    {
+        public static T AddPath<T>(this T argumentsBuilder, AbsolutePath path)
+            where T : IArgumentsBuilder
+        {
+            argumentsBuilder.AddPath(path.Value);
+
+            return argumentsBuilder;
+        }
+    }
+}
---
+++ source/R5T.Neapolis/Code/Extensions/NameValueExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using R5T.NetStandard.IO.Paths;
+
+
+namespace R5T.Neapolis
+{
+    public static class NameValueExtensions
+    {
+        public static NameValueArgument SetValuePath(this NameValueArgument nameValue, AbsolutePath path)
+        {
+            nameValue.SetValuePath(path.Value);
+
+            return nameValue;
+        }
+    }
+}
